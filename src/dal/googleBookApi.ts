92a9c5add3ad3f94c5eedadf4e3abb8e469dc3fa
Valery91-qw <vakriv91@gmail.com>
2021-08-13 11:18:22 +0300

    added types

--- src/dal/googleBookApi.ts
+++ src/dal/googleBookApi.ts
@@ -5,11 +5,31 @@ const API_KEY = process.env.REACT_APP_APIKEY || apiKey
 
 export const googleBookApi = {
     getBooks(searchString: string) {
-      return axios.get(`https://www.googleapis.com/books/v1/volumes`, {
+      return axios.get<ResponseType>(`https://www.googleapis.com/books/v1/volumes`, {
           params: {
               key: API_KEY,
               q: searchString,
           }
       })
+          .then(res => res)
     }
+}
+
+
+export type BookTypes = {
+    volumeInfo: {
+        authors: Array<string>
+        categories: Array<string>
+        description: string
+        imageLinks: {
+            smallThumbnail: string
+            thumbnail: string
+        }
+        pageCount: number
+    }
+}
+export type ResponseType = {
+    kind: string
+    totalItems: number
+    items: Array<BookTypes>
 }
