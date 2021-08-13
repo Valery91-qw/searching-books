290c5c1d8b8c29d7dd75723b4b17976c58fd319d
Valery91-qw <vakriv91@gmail.com>
2021-08-13 17:00:37 +0300

    layout search field

--- src/view/header/searchField/SearchField.test.tsx
+++ src/view/header/searchField/SearchField.test.tsx
@@ -3,10 +3,11 @@ import {SearchField} from "./SearchField";
 import userEvent from "@testing-library/user-event";
 
 describe('searchField component', () => {
-    test('The search field must implement one input field and an icon', () => {
+    test('The search field must implement one input field and an icon and button', () => {
         render(<SearchField /> )
         expect(screen.getByRole('textbox')).toBeInTheDocument()
         expect(screen.getByRole('img')).toBeInTheDocument()
+        expect(screen.getByRole('button')).toBeInTheDocument()
     })
     test('The input element must be selected by placeholder, must display the imprinted text', () => {
         render(<SearchField />)
--- src/view/header/searchField/SearchField.tsx
+++ src/view/header/searchField/SearchField.tsx
@@ -1,11 +1,13 @@
 import style from './SearchField.module.css'
-import image from '../../../logo.svg'
 
 export const SearchField = () => {
     return (
         <div className={style.input__wrapper}>
             <input className={style.input} type="text" placeholder="Search"/>
-            <img className={style.icon} style={{backgroundImage: `url(${image})`}} />
+            <button className={style.button}>
+            <img alt="search_icon" className={style.icon}
+                 src={"https://listimg.pinclipart.com/picdir/s/109-1093260_search-loop-svg-png-icon-free-download-524293.png"}/>
+            </button>
         </div>
     )
 }
