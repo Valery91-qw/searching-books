dbd37df191bbd7b179a94d2b2637e3b8e5e38eb6
Valery91-qw <vakriv91@gmail.com>
2021-08-17 15:38:40 +0300

    FetchBooks worker saga is tested

--- src/bll/books/books-operations.test.ts
+++ src/bll/books/books-operations.test.ts
@@ -1,8 +1,15 @@
 import {fetchBooks} from "./books-operations";
 import {watcher} from "../saga";
-import {takeEvery} from "redux-saga/effects";
+import {call, put, takeEvery} from "redux-saga/effects";
 import {books_constants} from "./books-constants";
+import {getBooks, putBooks} from "./books-actions";
+import {googleBookApi, ResponseType} from "../../dal/googleBookApi";
+import {AxiosResponse} from "axios";
+import {BookType} from "./books-model";
 
+jest.mock("../../dal/googleBookApi")
+
+const googleBookApiMock = googleBookApi as jest.Mocked<typeof googleBookApi>
 
 describe('redux saga', () => {
    test('the getBook action must be called with the correct worker', () => {
@@ -13,4 +20,46 @@ describe('redux saga', () => {
       expect(generator.next().done).toBeTruthy()
    })
 
+   test('The fetchBooks saga should pass the test correctly with the current test data', () => {
+
+      const resolve: AxiosResponse<ResponseType> = {
+         status: 200,
+         statusText: 'OK',
+         data: {
+            items: [
+               {volumeInfo : {
+                  authors: ['one', 'two'],
+                     title: 'some title',
+                     pageCount: 1,
+                     description: 'fake',
+                     categories: ['one', 'two'],
+                     imageLinks: {
+                     smallThumbnail: 'fake small',
+                        thumbnail: 'fake large',
+                     }
+                  }},
+            ],
+            totalItems: 1,
+            kind: 'none'
+         },
+         config: {
+
+         },
+         headers: '',
+         request: ''
+      }
+
+      const convertedType: Array<BookType> = [
+         {authors: 'one,two', categories: 'one,two', description: 'fake', imageLinks: {
+            smallThumbnail: 'fake small',
+               thumbnail: 'fake large',
+            }, title : 'some title', pageCount: 1}
+      ]
+
+      const generator = fetchBooks(getBooks('hello'))
+
+      expect(generator.next().value).toEqual(call(googleBookApiMock.getBooks, 'hello' ))
+
+      expect(generator.next(resolve).value).toEqual(put(putBooks(convertedType)))
+   })
 })
